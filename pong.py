ea94c6e28206d98241f6ecfc96c782f8ff85b0d4
BasiliskOps <noro.nero@gmail.com>
2021-08-07 12:14:58 -0500

    paddle one created

--- pong.py
+++ pong.py
@@ -8,6 +8,24 @@ win.setup(width=666, height=666)
 win.tracer(0)
 
 
+# Paddle One
+paddle_one = turtle.Turtle()
+paddle_one.speed(0)
+paddle_one.shape("square")
+paddle_one.color("white")
+paddle_one.shapesize(stretch_wid=5, stretch_len=1)
+paddle_one.penup()
+paddle_one.goto(-300, 0)
+
+
+# Paddle Two
+
+
+
+# Ball
+
+
+
 # Main Loop
 while True:
     win.update()
